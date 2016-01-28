5bdff4c12fb5f7a3ce135b3a64055e71119ff62c
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-01-28 14:08:55 +0100

    2nd

--- cppOving2/cppOving2/main.cpp
+++ cppOving2/cppOving2/main.cpp
@@ -10,17 +10,22 @@ using namespace std;
 void main() 
 {
 
+	//Nødvendige variabler
 	double samtaletid;
 	double a;
 	double b;
 
+	//Ber brukeren skrive inn gjennomsnittlig samtaletid.
 	cout << "Vennligst oppgi din gjennomsnittlige samtaletid pr. mnd:" << endl;
 	
+	//Leser hva brukeren skrev og lagrer det i variabelen samtaletid
 	cin >> samtaletid;
 
-	a = 50 + (samtaletid * 1);
+	//Formler for utregning av abbo a & b
+	a = 50 + samtaletid;
 	b = samtaletid * 2.50;
 
+	//Printer resultatet til console
 	cout << "Abonnement A blir † koste deg: " << a << ",-" << endl;
 	cout << "Abonnement B blir † koste deg: " << b << ",-" << endl;
 
